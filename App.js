6e1efd1c3b1dbfff706a459da719516fa0d62ec5
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-02-08 12:08:08 +0000

    square bounces upon touch

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import entities from './entities';
 import { GameEngine } from 'react-native-game-engine';
@@ -7,18 +7,22 @@ import Physics from './physics';
 
 export default function App() {
   const [running, setrunning] = useState(false);
+  useEffect(() => {
+    setrunning(true);
+  }, []);
   return (
     <View style={{flex: 1}}>
       <GameEngine
-      systems={[Physics]}
+      systems = {[Physics]}
       entities = {entities()}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0
-        }}
+      running = {running}
+      style = {{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+      }}
       >
       </GameEngine>
       <StatusBar style="auto" hidden={true} />
--- physics.js
+++ physics.js
@@ -2,7 +2,15 @@ import Matter from "matter-js";
 
 const Physics = (entities, { touches, time, dispatch }) => {
   let engine = entities.physics.engine;
-  Matter.Engine.update(engine, time.delta)
+
+  touches.filter(t => t.type === 'press').forEach(t => {
+    Matter.Body.setVelocity(entities.Bird.body, {
+      x: 0,
+      y: -8
+    })
+  })
+
+  Matter.Engine.update(engine, time.delta);
 
   return entities;
 }
